9dddd97507b7fdfd185cf28915d35b0c58d6be15
ly12lil <908931137@qq.com>
2019-01-08 15:34:45 +0800

    commit

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'qsl',
+    'database'        => '****',
     // 用户名
-    'username'        => 'qsl',
+    'username'        => '****',
     // 密码
-    'password'        => '908931137',
+    'password'        => '****',
     // 端口
     'hostport'        => '',
     // 连接dsn
